899539a1094d36eae0f0ffda7e517453b8cab58e
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2024-12-06 01:18:38 +0500

    dynamic-blog

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -7,7 +7,7 @@ export default function ContactForm() {
           Get in Touch with Us!
           </h2>
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-            Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.
+          Got a question about your next adventure? Want to share your travel experiences with us? Need details about our travel packages? Let us know.
           </p>
           <form action="#" className="space-y-8">
             <div>
